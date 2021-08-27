4a20adfff914fdcfcdb56c3a5dd85c5ddb359c91
Rodolphe Irany <roudyirany@me.com>
2021-08-27 22:05:42 +0100

    Implement view holder and adapters

---
+++ app/src/main/java/com/sword/demo/ui/home/adapter/BreedListItemAdapter.kt
@@ -0,0 +1,35 @@
+package com.sword.demo.ui.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sword.demo.R
+import com.sword.demo.network.models.Breed
+
+class BreedListItemAdapter : RecyclerView.Adapter<BreedItemViewHolder>() {
+
+    private val items = mutableListOf<Breed>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BreedItemViewHolder {
+        return BreedItemViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.layout_breed_list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: BreedItemViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount() = items.size
+
+    fun update(items: List<Breed>) {
+        this.items.clear()
+        add(items)
+    }
+
+    fun add(items: List<Breed>) {
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
+}
