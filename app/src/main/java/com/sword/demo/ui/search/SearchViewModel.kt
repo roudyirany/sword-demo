b807080cc78aa99ccdb56d2c1eb3071e1417dd91
Rodolphe Irany <roudyirany@me.com>
2021-08-28 12:49:18 +0100

    Add search breed method to search view model

--- app/src/main/java/com/sword/demo/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/sword/demo/ui/search/SearchViewModel.kt
@@ -1,16 +1,22 @@
 package com.sword.demo.ui.search
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sword.demo.network.ApiService
+import com.sword.demo.network.models.Breed
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor() : ViewModel() {
+class SearchViewModel @Inject constructor(
+    private val apiService: ApiService
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    fun searchBreed(name: String): Observable<List<Breed>> {
+        return apiService.searchBreed(name)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
     }
-    val text: LiveData<String> = _text
 }
