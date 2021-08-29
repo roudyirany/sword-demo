6cff19370a7781393f22ea5a51850b8d4e2bd0d0
Rodolphe Irany <roudyirany@me.com>
2021-08-29 20:13:27 +0100

    Integrate safe args with viewmodel and display info in details fragment

--- app/src/main/java/com/sword/demo/ui/fragment/details/DetailsFragment.kt
+++ app/src/main/java/com/sword/demo/ui/fragment/details/DetailsFragment.kt
@@ -5,15 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
-import com.sword.demo.R
+import androidx.fragment.app.viewModels
 import com.sword.demo.databinding.FragmentDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment() {
 
-    private val viewModel: DetailsViewModel by hiltNavGraphViewModels(R.navigation.main_navigation)
+    private val viewModel: DetailsViewModel by viewModels()
     private var _binding: FragmentDetailsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -26,5 +25,14 @@ class DetailsFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.breedNameValue.text = viewModel.name
+        binding.breedCategoryValue.text = viewModel.category
+        binding.breedOriginValue.text = viewModel.origin
+        binding.breedTemperamentValue.text = viewModel.temperament
     }
 }
--- app/src/main/java/com/sword/demo/ui/fragment/details/DetailsViewModel.kt
+++ app/src/main/java/com/sword/demo/ui/fragment/details/DetailsViewModel.kt
@@ -1,9 +1,23 @@
 package com.sword.demo.ui.fragment.details
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.sword.demo.network.models.Breed
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailsViewModel @Inject constructor() : ViewModel() {
+class DetailsViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val breed: Breed? = savedStateHandle["breed"]
+
+    val name = breed?.name
+
+    val category = breed?.category
+
+    val origin = breed?.origin
+
+    val temperament = breed?.temperament
 }
