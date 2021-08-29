6ca6899175c7fdc1b133e405cecf01bf03f4d209
Rodolphe Irany <roudyirany@me.com>
2021-08-29 19:49:17 +0100

    Parcelize Breed model

--- app/src/main/java/com/sword/demo/network/models/Image.kt
+++ app/src/main/java/com/sword/demo/network/models/Image.kt
@@ -1,7 +1,10 @@
 package com.sword.demo.network.models
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Image(
     @SerializedName("url") val url: String
-)
+) : Parcelable
