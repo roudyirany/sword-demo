0d39db67717cac7e549798023332fd1bacc69361
Rodolphe Irany <roudyirany@me.com>
2021-08-29 18:27:14 +0100

    Add details activity

---
+++ app/src/main/java/com/sword/demo/DetailsActivity.kt
@@ -0,0 +1,16 @@
+package com.sword.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sword.demo.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
