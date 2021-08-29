f626647fc9e6a84a222f17177395470c857ded7c
Rodolphe Irany <roudyirany@me.com>
2021-08-29 19:04:51 +0100

    Add details fragment

--- app/src/main/java/com/sword/demo/ui/activity/details/DetailsActivity.kt
+++ app/src/main/java/com/sword/demo/ui/activity/details/DetailsActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sword.demo.R
 import com.sword.demo.databinding.ActivityDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
---
+++ app/src/main/java/com/sword/demo/ui/fragment/details/DetailsFragment.kt
@@ -0,0 +1,30 @@
+package com.sword.demo.ui.fragment.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
+import com.sword.demo.R
+import com.sword.demo.databinding.FragmentDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailsFragment : Fragment() {
+
+    private val viewModel: DetailsViewModel by hiltNavGraphViewModels(R.navigation.details_navigation)
+    private var _binding: FragmentDetailsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
